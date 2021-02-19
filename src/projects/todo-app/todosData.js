7df1ee1756c1845042395cc7eb03a4c5e563e6f0
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-19 15:14:59 +0100

    todo data

--- src/projects/todo-app/todosData.js
+++ src/projects/todo-app/todosData.js
@@ -1,43 +1,23 @@
 const todosData = [
     {
         id: 1,
-        text: "Take out the trash",
+        text: "tu was zu tun",
         completed: true
     },
     {
         id: 2,
-        text: "Grocery shopping",
+        text: "geh wohin zu gehen",
         completed: false
     },
     {
         id: 3,
-        text: "Clean gecko tank",
+        text: "mach das Zimmer sauber",
         completed: false
     },
     {
         id: 4,
-        text: "Mow lawn",
+        text: "Besuch dein Mama",
         completed: true
-    },
-    {
-        id: 5,
-        text: "Catch up on Arrested Development",
-        completed: false
-    },
-    {
-        id: 6,
-        text: "Go there",
-        completed: true
-    },
-    {
-        id: 7,
-        text: "Come here",
-        completed: false
-    },
-    {
-        id: 8,
-        text: "Try it again",
-        completed: false
     }
 ]
 
