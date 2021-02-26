531ca8f07ab7bae2117f5899f4398074c0a2d2de
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-26 13:08:09 +0100

    counter

--- src/projects/simple-counter/App.js
+++ src/projects/simple-counter/App.js
@@ -1,13 +1,11 @@
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import randomColor from "randomcolor"
-import { useEffect } from 'react';
 
 function App() {
     const [count1, setCount1] = useState(0)
     const [count2, setCount2] = useState(0)
-    const [style, setStyle] = useState({ display: "block" })
-    const [style1, setStyle1] = useState({ display: "none" })
     const [color, setColor] = useState("")
+    const [finish, setFinish] = useState(false)
 
     var color1 = randomColor()
 
@@ -18,11 +16,15 @@ function App() {
     }
 
     const deleteAll = () => {
-        let style = { display: "none" }
-        setStyle(style)
-        let style1 = { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '202px', color: color1 }
-        setStyle1(style1)
+        setFinish(true)
     }
+
+    useEffect(() => {
+        setInterval(() => {
+            setCount1(i => i + 1)
+        }, 1000);
+    }, [])
+
 
     useEffect(() => {
         setColor(randomColor())
@@ -30,16 +32,20 @@ function App() {
 
     return (
         <div style={{ textAlign: 'center' }}>
-            <div style={style}>
-                <h1 style={{ color: color }} >{count1}</h1>
-                <button style={{ backgroundColor: "blue", color: 'whitesmoke' }} onClick={() => setCount1(prevCount => prevCount + 1)}>Plus!</button>
-                <br /><button onClick={deleteAll} style={{ borderRadius: '50%', backgroundColor: "red", color: 'whitesmoke' }}>delete</button><br />
-                <button style={{ backgroundColor: "yellow" }} onClick={decrement}>Minus!</button>
-                <h1>  {count2}</h1>
-            </div>
-            <div style={style1}>
-                <h1>Good by!</h1>
-            </div>
+            {!finish ?
+                <div >
+                    <h1 style={{ color: color }} >{count1}</h1>
+                    <button style={{ backgroundColor: "blue", color: 'whitesmoke' }} onClick={() => setCount1(prevCount => prevCount + 1)}>Plus!</button>
+                    <br /><button onClick={deleteAll} style={{ borderRadius: '50%', backgroundColor: "red", color: 'whitesmoke' }}>delete</button><br />
+                    <button style={{ backgroundColor: "yellow" }} onClick={decrement}>Minus!</button>
+                    <h1>  {count2}</h1>
+                </div>
+                :
+
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '202px', color: color1 }}>
+                    <h1>Good by!</h1>
+                </div>
+            }
         </div>
     )
 }
