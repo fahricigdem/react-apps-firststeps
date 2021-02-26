d16f8d8603b7c4f809c3caffe491dcd87569b9a3
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-26 08:22:50 +0100

    counter

--- src/projects/simple-counter/App.js
+++ src/projects/simple-counter/App.js
@@ -1,12 +1,15 @@
 import React, { Component, useState } from 'react';
+import randomColor from "randomcolor"
+import { useEffect } from 'react';
 
 function App() {
     const [count1, setCount1] = useState(0)
     const [count2, setCount2] = useState(0)
     const [style, setStyle] = useState({ display: "block" })
     const [style1, setStyle1] = useState({ display: "none" })
+    const [color, setColor] = useState("")
 
-
+    var color1 = randomColor()
 
     const decrement = () => {
         // let value = count2 - 1
@@ -17,16 +20,20 @@ function App() {
     const deleteAll = () => {
         let style = { display: "none" }
         setStyle(style)
-        let style1 = { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '202px', border: '3px solid green' }
+        let style1 = { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '202px', color: color1 }
         setStyle1(style1)
     }
+
+    useEffect(() => {
+        setColor(randomColor())
+    }, [count1])
 
     return (
         <div style={{ textAlign: 'center' }}>
             <div style={style}>
-                <h1 >{count1}</h1>
-                <button style={{ backgroundColor: "blue" }} onClick={() => setCount1(prevCount => prevCount + 1)}>Plus!</button>
-                <br /><button onClick={deleteAll} style={{ borderRadius: '50%', backgroundColor: "red" }}>delete</button><br />
+                <h1 style={{ color: color }} >{count1}</h1>
+                <button style={{ backgroundColor: "blue", color: 'whitesmoke' }} onClick={() => setCount1(prevCount => prevCount + 1)}>Plus!</button>
+                <br /><button onClick={deleteAll} style={{ borderRadius: '50%', backgroundColor: "red", color: 'whitesmoke' }}>delete</button><br />
                 <button style={{ backgroundColor: "yellow" }} onClick={decrement}>Minus!</button>
                 <h1>  {count2}</h1>
             </div>
