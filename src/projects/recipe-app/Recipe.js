5a2bb5da7577f8afea112085758bd9ceab96a305
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-28 11:32:53 +0100

    meals

--- src/projects/recipe-app/Recipe.js
+++ src/projects/recipe-app/Recipe.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 function Recipe({ label, calories, image }) {
     return (
         <div>
-            <h1>{label}</h1>
-            <h2>calories: {Math.round(calories)}</h2>
+            <h1 style={{ color: 'white' }}>{label}</h1>
+            <h2 style={{ color: '#ffbe0f' }}>calories: {Math.round(calories)}</h2>
             <img src={image} />
         </div>
     )
