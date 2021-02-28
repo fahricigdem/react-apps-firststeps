54192188596bfcecd18df951312ec77e8c400417
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-28 13:48:15 +0100

    meal

--- src/projects/recipe-app/App.js
+++ src/projects/recipe-app/App.js
@@ -13,31 +13,45 @@ const App = () => {
     const APP_KEY = "a43678d2012a0b9c6d815c7bbbfea1ef"
 
     const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+
+        const response = await fetch(`https://api.edamam.com/search?q=${filters.name}&app_id=${APP_ID}&app_key=${APP_KEY}`)
         const data = await response.json()
         setRecipes(data.hits)
+
     }
 
     useEffect(() => {
+
         getRecipes()
-    }, [])
+
+    }, [filters])
 
     // recipes.map(i => console.log(i.recipe.label))
 
     const handleName = (e) => {
+
         setName(e.target.value.toLowerCase())
+
     }
 
     const handleCalories = (e) => {
+
         setCalories(e.target.value)
+
     }
 
     const handleSubmit = (e) => {
+
         if (calories == "") {
+
             setFilters({ name: name, calories: 10000 })
+
         } else {
+
             setFilters({ name: name, calories: calories })
+
         }
+
         e.preventDefault();
     }
 
@@ -62,7 +76,6 @@ const App = () => {
 
     console.log("results: ", results)
 
-
     const style = {
         display: 'flex', flexDirection: "column", justifyContent: 'space-around', alignItems: 'center', flexFlow: 'row wrap'
     }
@@ -72,11 +85,28 @@ const App = () => {
 
             <form className="search-form" onSubmit={handleSubmit}>
 
-                <input type="text" className="search-bar" placeholder="Search by Name!..." onChange={handleName} /><br />
-                <input type="text" className="search-bar" placeholder="Search by Max Calories!..." onChange={handleCalories} /><br />
+                <input type="text" className="search-bar" placeholder="Search! chicken etc..." onChange={handleName} value={name} /><br />
+                {/* <input type="text" className="search-bar" placeholder="Filter by Max Calories!..." onChange={handleCalories} /><br /> */}
+                <select
+                    name="favColor"
+                    // value={filters.calories}
+                    onChange={handleCalories}
+                    className="form-select form-select-sm"
+
+                >
+                    <option selected>Filter by Max Calories!...</option>
+                    <option value="10000">reset</option>
+                    <option value="5000">5000</option>
+                    <option value="4000">4000</option>
+                    <option value="3000">3000</option>
+                    <option value="2000">2000</option>
+                    <option value="1000">1000</option>
+
+                </select><br />
+
                 <button className="btn btn-success" style={{ borderRadius: "50%" }} >Submit</button></form><br />
 
-            {results < 10 ? <h2><span className="badge badge-danger">results: {results}</span> </h2> : <h2><span className="badge badge-warning">All Products</span></h2>}<br />
+            {results < 10 ? <h2><span className="badge badge-danger">results: {results}</span> </h2> : <h2><span className="badge badge-warning">All Products: {results}  </span></h2>}<br />
 
             <div style={style} >
                 {data}  </div></div>
--- src/projects/recipe-app/Recipe.js
+++ src/projects/recipe-app/Recipe.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 function Recipe({ label, calories, image }) {
     return (
-        <div>
-            <h1 style={{ color: 'white' }}>{label}</h1>
+        <div style={{ maxWidth: "300px" }}>
+            <div style={{ height: '100px', alignItems: 'center', overflow: 'hidden' }} ><h1 style={{ color: 'white' }}>{label}</h1></div>
             <h2 style={{ color: '#ffbe0f' }}>calories: {Math.round(calories)}</h2>
             <img src={image} />
-        </div>
+        </div >
     )
 }
 
