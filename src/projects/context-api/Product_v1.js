435956ac8ba55de230624b86640e5fab46217426
Fahri Cigdem <fahricigdem34@gmail.com>
2021-03-06 00:23:21 +0100

    context api local

--- src/projects/context-api/Product_v1.js
+++ src/projects/context-api/Product_v1.js
@@ -1,10 +1,37 @@
 import React from "react"
-import { useState, useContext } from "react"
+import { useState, useEffect, useContext } from "react"
 import { ProductContext } from "./ProductContext"
 
 function Product() {
 
     const [products, setProducts] = useContext(ProductContext)
+
+    useEffect(() => {
+        getLocalTodos()
+    }, [])
+
+    useEffect(() => {
+        saveLocalTodos()
+    }, [products])
+
+    const saveLocalTodos = () => {
+
+        localStorage.setItem("products", JSON.stringify(products))
+
+        // console.log("stateData (in saveLocalTodos) ", this.state.data)
+        // console.log("todoLocal (in saveLocalTodos) ", JSON.parse(localStorage.getItem("todos")))
+    }
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem("products") === null) {
+            localStorage.setItem("products", JSON.stringify([]))
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("products"))
+            // console.log("in getLocalTodos todoLocal", todoLocal)
+            setProducts(todoLocal)
+        }
+    }
+
 
     return (
         <div>
