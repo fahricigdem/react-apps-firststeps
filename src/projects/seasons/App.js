25b7140ba5e3f60682eebda94e35300886d4a07d
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-04-05 17:17:57 +0200

    season-display completed

--- src/projects/seasons/App.js
+++ src/projects/seasons/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount() {
         navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-                console.log("error:", err.message)
-            }
-        );
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        )
     }
 
+    renderContent() {
+        return (
+            this.state.lat ? <SeasonDisplay lat={this.state.lat} />
+                : this.state.errorMessage ? <div className="season-display">{this.state.errorMessage}</div>
+                    : <div style={{ position: 'absolute', width: '100%', height: '200px' }}>
+                        <Spinner message='waiting for location data' />
+                    </div>//spinner should smaller because we have here more than one project //other option: <div><i class="huge spinner loading icon"></i> <h1>loading...</h1></div>
+        )
+    }
 
     render() {
-
-        return (this.state.lat ? <div>{this.state.lat}</div> : <div>Latitide:{this.state.errorMessage}</div>);
+        return (
+            <div className="......">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
--- src/projects/seasons/SeasonDisplay.js
+++ src/projects/seasons/SeasonDisplay.js
@@ -1,6 +1,32 @@
-const SeasonDisplay = () => {
+import './SeasonDisplay.css';
+import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: "ohh!.. let's go to beach",
+        iconName: "sun"
+    },
+    winter: {
+        text: 'Burr!.. it is chilly',
+        iconName: 'snowflake'
+    }
+}
+const getSeason = (lat, month) => {
+    if (month > 5 && month < 10) {
+        return lat < 0 ? 'winter' : 'summer';
+    }
+    else return lat > 0 ? 'winter' : 'summer'
+}
+
+const SeasonDisplay = (props) => {
+    let season = getSeason(props.lat, new Date().getMonth())
+    const { text, iconName } = seasonConfig[season]
     return (
-        <div>Season Display</div>
+        <div className={`season-display ${season}`} >
+            <i class={`icon-left huge ${iconName} icon`}></i>
+            <h1>{text}<span>*Your Latitude:{props.lat.toFixed(1)} and {new Date().toLocaleString('default', { month: 'long' })} so it is {season}</span></h1>
+            <i class={`icon-right huge ${iconName} icon`}></i>
+        </div>
     );
 }
 
