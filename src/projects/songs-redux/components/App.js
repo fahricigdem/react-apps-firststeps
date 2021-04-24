b87fd364ca26f7dccd8cfff7a430c71d84832793
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-04-24 04:49:51 +0200

    redux song app over

--- src/projects/songs-redux/components/App.js
+++ src/projects/songs-redux/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container grid">
+      <div className="ui row" style={{ marginTop: '20px', minHeight: '40vh' }}>
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+        <i>*Redux Application</i>
+      </div>
+    </div>
+  );
 };
 
 export default App;
